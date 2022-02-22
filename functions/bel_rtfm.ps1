7c1b8c057e7d17fc49518225e24cd5f3f0f52ad7
brandon77763 <52871954+brandon77763@users.noreply.github.com>
2022-02-22 15:27:36 -0500

    .

--- functions/bel_rtfm.ps1
+++ functions/bel_rtfm.ps1
@@ -1,5 +1,5 @@
 function Show-BEL_RTFM(){
-    Write-Host" 
+    Write-Host "
                                                                                                                           *.@@@.,  *@( (,,
                                                                                                                  %@.( %.  %@..(@,*@%/./@
                                                                                                        .#@@@*#  .@#(. @@. (*/ &@@@( @@@@& @
