2b38ab81482ac6695b969a96a2a6a4d554f57daa
brandon77763 <52871954+brandon77763@users.noreply.github.com>
2022-02-22 11:59:45 -0500

    .

--- build/bel_update.ps1
+++ build/bel_update.ps1
@@ -1,3 +1,4 @@
 Install-Module -Name InstallModuleFromGitHub
+Remove-Module BELPSTools
 Install-ModuleFromGitHub -GitHubRepo brandon77763/BELPSTools
 Rename-Item -Path ".\BELPSTools-main" -NewName ".\BELPSTools"
