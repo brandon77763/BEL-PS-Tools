9aabb7797c2e1e521da12f306d85635712155640
brandon77763 <52871954+brandon77763@users.noreply.github.com>
2022-02-21 19:35:10 -0500

    2/21/22 7:35PM EST

--- bel-ps-module.psm1
+++ bel-ps-module.psm1
@@ -1,5 +1,21 @@
+<#
+ .Synopsis
+  Displays a visual representation of a calendar.
 
+ .Description
+  Displays a visual representation of a calendar. This function supports multiple months
+  and lets you highlight specific date ranges or days.
+
+ .Example
+   # Show a default display of this month.
+   Show-Calendar
+#>
 #Update 
-function BEL-Update {
+
+function Get-BEL-Update {
+    powershell  ".\build\bel-update.ps1"
+}
+
+function Show-BEL-RTFM {
     powershell  ".\build\bel-update.ps1"
 }
