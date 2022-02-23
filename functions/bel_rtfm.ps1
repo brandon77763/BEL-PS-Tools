ce4c4e4e13847d201ce6b91b85e7d60bcc2915c2
brandon77763 <52871954+brandon77763@users.noreply.github.com>
2022-02-22 22:36:50 -0500

    .

--- functions/bel_rtfm.ps1
+++ functions/bel_rtfm.ps1
@@ -32,24 +32,6 @@ function Show-BEL_RTFM(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     " -ForegroundColor red
 
 Write-Host "For A List Of Commands Run: " -ForegroundColor Blue -NoNewline; Write-Host "Get-BEL_Commands" -ForegroundColor Green -NoNewline;
