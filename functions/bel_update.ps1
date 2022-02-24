342a6fbf4a79242f31c5c52d8c1026cab04e092a
brandon77763 <52871954+brandon77763@users.noreply.github.com>
2022-02-23 20:40:40 -0500

    .

--- functions/bel_update.ps1
+++ functions/bel_update.ps1
@@ -1,19 +1,21 @@
 function Get-BEL_Update(){
 write-Host Updating GitHub Module..... 
 Install-Module -Name InstallModuleFromGitHub -RequiredVersion 0.3
-Start-Sleep -s 4
+Start-Sleep -s 5
 write-Host Removing Old BELPSTools Module..... 
 Remove-Module BELPSTools
-Start-Sleep -s 4
+Start-Sleep -s 5
 write-Host Downloading New BELPSTools Module..... 
 Install-ModuleFromGitHub -GitHubRepo brandon77763/BELPSTools
-Start-Sleep -s 4
+Start-Sleep -s 15
 write-Host Removing Old BELPSTools Files Module..... 
 Remove-Item '.\BELPSTools' -Recurse
 Start-Sleep -s 5
 write-Host Configuring New BELPSTools Files Module..... 
 Rename-Item -Path ".\BELPSTools-main" -NewName ".\BELPSTools"
-Start-Sleep -s 4
+Start-Sleep -s 5
 write-Host Tasks Completed..... 
+Start-Sleep -s 3
+Exit
 }
 
