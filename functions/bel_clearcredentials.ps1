e56a09d7d301627e581913e6718a936694727434
brandon77763 <52871954+brandon77763@users.noreply.github.com>
2022-03-11 15:46:03 -0500

    .

--- functions/bel_clearcredentials.ps1
+++ functions/bel_clearcredentials.ps1
@@ -1,6 +1,6 @@
 function Invoke-BEL_ClearCredentials(){
     $datapath = $PSScriptRoot + "\data\"
-    $command = $datapath + 'bel_clearcredential.bat'
+    $command = $datapath + 'bel_clearcredential.cmd'
     write-Host($command)
     cmd.exe /c $command
 }
