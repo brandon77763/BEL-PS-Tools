3524599d1e5ef036971c36285e24a08b4cc78196
brandon77763 <52871954+brandon77763@users.noreply.github.com>
2022-09-28 12:38:12 -0400

    Update bel_installmodules.ps1

--- functions/bel_installmodules.ps1
+++ functions/bel_installmodules.ps1
@@ -1,7 +1,7 @@
 function Get-BEL_InstallModules(){
    Install-Module -Name PowerShellGet -Force -AllowClobber
-   Install-Module -Name ExchangeOnlineManagement
-   Install-Module -Name InstallModuleFromGitHub
+   Install-Module -Name ExchangeOnlineManagement -Force
+   Install-Module -Name InstallModuleFromGitHub -Force
    Install-Module AzureAD -Force
    Install-Module -Name MicrosoftTeams -Force -AllowClobber
 }
