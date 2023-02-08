3eb115c0a6da08d53dbc81e0bff02da7f3128ff4
brandon77763 <52871954+brandon77763@users.noreply.github.com>
2023-02-08 14:28:12 -0500

    Update bel_createoutlookcalendar.ps1

--- functions/bel_createoutlookcalendar.ps1
+++ functions/bel_createoutlookcalendar.ps1
@@ -10,10 +10,12 @@ $email
 $alias
 #Room 
 $Capacity
+$CountryOrRegion
 $Building
 $Floor
 $FloorLabel
 $Label
+
 
 
 
@@ -37,24 +39,42 @@ get-ExchangeOnlineConnection
 
 function build-build{
     try {
+        Write-Host "Creating Account..." -ForegroundColor Blue
         New-Mailbox -MicrosoftOnlineServicesID $email -Alias $alias -Name "$roomname" -DisplayName "$displayname" -Room -EnableRoomMailboxAccount $true -RoomMailboxPassword (ConvertTo-SecureString -String $pwd1 -AsPlainText -Force)
         set-sleep(4)
+        Write-Host "Setting Mailbox Permissions..." -ForegroundColor Blue
         Set-MailboxFolderPermission -Identity $alias':\Calendar' -User Default -AccessRights LimitedDetails
         set-sleep(4)
+        Write-Host "Setting Mailbox Location Info..." -ForegroundColor Blue
+        Set-Place -Identity "$alias" -Building "$Building" -Capacity "$Capacity" -CountryOrRegion $CountryOrRegion -Floor "$Floor" -FloorLabel "$FloorLabel" -Label "$Label"
+        set-sleep(4)
+        Write-Host "Applying Calendar Proccessing Variables..." -ForegroundColor Blue
+        Set-CalendarProcessing -Identity "$alias" -AutomateProcessing AutoAccept -AddOrganizerToSubject $false -DeleteComments $false -DeleteSubject $false -ProcessExternalMeetingMessages $true -RemovePrivateProperty $false -AddAdditionalResponse $true -AdditionalResponse "This is a Microsoft Teams Meeting room!"
+        set-sleep(4)
+        Write-Host "###########Gathering Account Info###########" -ForegroundColor Blue
+        Get-CalendarProcessing -Identity "$alias" | fl
+        set-sleep(4)
         Get-MailboxFolderPermission -Identity $alias':\Calendar' | fl
-        set-sleep(4)
-        Set-Place -Identity "$alias" -Building "$Building" -Capacity "$Capacity" -CountryOrRegion ES -Floor "$Floor" -FloorLabel "$FloorLabel" -Label "$Label"
-        set-sleep(4)
-        Set-CalendarProcessing -Identity "$alias" -AutomateProcessing AutoAccept -AddOrganizerToSubject $false -DeleteComments $false -DeleteSubject $false -ProcessExternalMeetingMessages $true -RemovePrivateProperty $false -AddAdditionalResponse $true -AdditionalResponse "This is a Microsoft Teams Meeting room!"   
         set-sleep(4) 
         if(![string]::IsNullOrEmpty($email)){
             set-sleep(10)
+            Write-Host "DisablePasswordExpiration..." -ForegroundColor Blue
             Set-AzureADUser -ObjectID '$email' -PasswordPolicies DisablePasswordExpiration
         }
     }
     catch {
         Write-Host "An error occurred:"
         Write-Host $_
+    }
+    $A1 = Read-Host "Do you want to create another room? Type(yes or no) "
+    while("yes","no" -notcontains $AV)
+    {
+        $A1 = Read-Host "Do you want to create another room? Type(yes or no) "
+    }
+    if($A1 -contains "no"){
+        exit
+    }else {
+        get-inputbox
     }
    
 }
@@ -67,6 +87,8 @@ function get-inputbox{
     $Building = Read-Host -Prompt "Please enter building and or site "
     $Floor = Read-Host -Prompt "Please enter the floor the room is on "
     $FloorLabel = Read-Host -Prompt "Please enter the floor label "
+    $Label = Read-Host -Prompt "Please enter the floor label "
+    $CountryOrRegion = Read-Host -Prompt "Please enter the Country or Region EX: US, SE, DE, CH, CA "
 
 
     $pwd1 = Read-Host -Prompt "Enter the new confrence room password" -AsSecureString
@@ -128,8 +150,16 @@ function get-inputbox{
         build-build
     }else {
         Write-Host "You have chosen to cancel the creation of this account. Now ending this script." -ForegroundColor Red
-        Pause
-        exit
+        $A1 = Read-Host "Do you want to create another room? Type(yes or no) "
+        while("yes","no" -notcontains $AV)
+        {
+            $A1 = Read-Host "Do you want to create another room? Type(yes or no) "
+        }
+        if($A1 -contains "no"){
+            exit
+        }else {
+            get-inputbox
+        }
     }
 
     pause
