7f6ba9af9e543f9526228ac4098a6cf55934affd
brandon77763 <52871954+brandon77763@users.noreply.github.com>
2023-02-28 12:00:44 -0500

    Update bel_UPNandSIPMatch.ps1

--- functions/bel_UPNandSIPMatch.ps1
+++ functions/bel_UPNandSIPMatch.ps1
@@ -1,9 +1,15 @@
-# Get all users from Active Directory
-$users = Get-ADUser -Filter * -Properties SIPAddress, UserPrincipalName
+# Connect to Azure AD
+Connect-AzureAD
+
+# Get all users from Azure AD
+$users = Get-AzureADUser -All $true | Select-Object DisplayName, UserPrincipalName, Mail, SignInNames
 
 # Loop through each user and check for matching SIP and UPN
 foreach ($user in $users) {
-    if ($user.SIPAddress -eq $user.UserPrincipalName) {
-        Write-Host "User $($user.Name) has matching SIP and UPN: $($user.SIPAddress)"
+    # Get the user's SIP address from the SignInNames array
+    $sipAddress = $user.SignInNames | Where-Object { $_.Type -eq 'sip' } | Select-Object -ExpandProperty Value
+    
+    if ($sipAddress -eq $user.UserPrincipalName) {
+        Write-Host "User $($user.DisplayName) has matching SIP and UPN: $($sipAddress)"
     }
 }
